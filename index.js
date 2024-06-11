516e84c950e80b37df252fc78504f5a4697c00e6
Coderperson101 <richardtbohnen@yahoo.com>
2024-06-11 15:09:57 +0200

    updated dyncamic port

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const app = express();
 var morgan = require('morgan');
 var finalhandler = require('finalhandler');
 
-
 let persons = [
 	{ "id": 1, "name": "Arto Hellas", "number": "040-123456" },
 	{ "id": 2, "name": "Ada Lovelace", "number": "39-44-5323523" },
@@ -13,20 +12,20 @@ let persons = [
 
 app.use(express.static('dist'));
 
-// const requestLogger = (request, response, next) => {
-// 	console.log('Method:', request.method);
-// 	console.log('Path:  ', request.path);
-// 	console.log('Body:  ', request.body);
-// 	console.log('---');
-// 	next();
-// };
-
 const cors = require('cors');
 
 app.use(cors());
 
+const requestLogger = (request, response, next) => {
+	console.log('Method:', request.method);
+	console.log('Path:  ', request.path);
+	console.log('Body:  ', request.body);
+	console.log('---');
+	next();
+};
+
 app.use(express.json());
-// app.use(requestLogger);
+app.use(requestLogger);
 
 const unknownEndpoint = (request, response) => {
 	response.status(404).send({ error: 'unknown endpoint' });
@@ -101,9 +100,9 @@ app.delete('/api/persons/:id', (request, response) => {
 
 app.use(unknownEndpoint);
 
-app.listen(3001);
+// app.listen(3001);
 
-// const PORT = process.env.PORT || 3001;
-// app.listen(PORT, () => {
-// 	console.log(`Server running on port ${PORT}`);
-// });
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`);
+});
