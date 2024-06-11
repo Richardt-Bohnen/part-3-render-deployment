28b5dce43f9e15ef9b6f36a502033372f45a2fae
Coderperson101 <richardtbohnen@yahoo.com>
2024-06-11 15:03:44 +0200

    redid using express library

--- build/index.js
+++ build/index.js
@@ -1,142 +1,109 @@
+const express = require('express');
+const app = express();
+var morgan = require('morgan');
 var finalhandler = require('finalhandler');
-const http = require('http');
-const url = require('url');
-const jsonServer = require('./jsonServer.js'); // mock database server (must `require`. Doesn't work in package.json).
-var morgan = require('morgan'); // handled inside the `addEntry` function
 
-const app = http.createServer(async (req, res) => {
-	const uri = url.parse(req.url).pathname;
-	const lastSegment = Number(uri.split('/').at(-1)); // get last url path segment and check if type Number
 
-	switch (uri) {
-		case "/api/persons":
-			if (req.method === "GET") getAll();
-			else if (req.method === "POST") addEntry();
-			return;
-		case `/api/persons/${lastSegment}`:
-			if (req.method === "GET") getEntry();
-			else if (req.method === "DELETE") deleteEntry();
-			return;
-		case `/api/info`:
-			getInfo();
-			return;
-		case "/favicon.ico": // remove annoying favicon console 404
-			res.writeHead(200, { 'Content-Type': 'text/html' });
-			res.end();
-			return;
-		default:
-			pathNotFound();
-	}
+let persons = [
+	{ "id": 1, "name": "Arto Hellas", "number": "040-123456" },
+	{ "id": 2, "name": "Ada Lovelace", "number": "39-44-5323523" },
+	{ "id": 3, "name": "Dan Abramov", "number": "12-43-234345" },
+	{ "id": 4, "name": "Mary Poppendieck", "number": "39-23-6423122" },
+];
 
-	async function getAll() {
-		try {
-			const response = await fetch('http://localhost:3002/api/persons', {
-				method: "GET",
-				headers: { "Content-Type": "application/json" }
-			});
-			if (response.status !== 200) {
-				throw new Error();
-			}
-			const data = await response.json();
-			res.writeHead(200, { 'Content-Type': 'application/json' });
-			res.write(JSON.stringify(data));
-			res.end();
-		} catch (error) { pathNotFound(); }
-	}
+app.use(express.static('dist'));
 
-	async function getEntry() {
-		try {
-			const response = await fetch('http://localhost:3002/api/persons/' + lastSegment, {
-				method: "GET",
-				headers: { "Content-Type": "application/json" }
-			});
-			if (response.status !== 200) {
-				throw new Error();
-			}
-			const data = await response.json();
-			res.writeHead(200, { 'Content-Type': 'application/json' });
-			res.write(JSON.stringify(data));
-			res.end();
-		} catch (error) { pathNotFound(); }
-	}
+// const requestLogger = (request, response, next) => {
+// 	console.log('Method:', request.method);
+// 	console.log('Path:  ', request.path);
+// 	console.log('Body:  ', request.body);
+// 	console.log('---');
+// 	next();
+// };
 
-	async function deleteEntry() {
-		try {
-			const response = await fetch('http://localhost:3002/api/persons/' + lastSegment, {
-				method: "DELETE",
-				headers: { "Content-Type": "application/json" }
-			});
-			if (response.status !== 204) {
-				throw new Error();
-			}
-			res.writeHead(204);
-			res.end();
-		} catch (error) { pathNotFound(); }
-	}
+const cors = require('cors');
 
-	async function addEntry() {
-		const newPerson = {
-			id: Math.round(Math.random() * 1000000),
-			name: "John Doe",
-			number: Math.round(Math.random() * 10000000000000).toString()
-		};
-		try {
-			const response = await fetch('http://localhost:3002/api/persons', {
-				method: "POST",
-				headers: { "Content-Type": "application/json" },
-				body: JSON.stringify(newPerson)
-			});
-			const data = await response.json();
-			if (response.status === 400) {
-				throw new Error(data.message); // name already exists or no name of number provided. Handled on line 61 in jsonServer.js
-			}
+app.use(cors());
 
-			var done = finalhandler(req, res);
-			var logger = morgan(':method :url :status :res[content-length] - :response-time ms :personAdded');
-			morgan.token('personAdded', function getPersonAdded(req) {
-				const personAdded = data.find(person => person.name === newPerson.name);
-				const { id, ...personDetails } = personAdded; // get person without  id prop for person details
-				return JSON.stringify(personDetails);
-			});
-			logger(req, res, function (err) { // morgan
-				if (err) return done(err);
-				res.writeHead(200, { 'Content-Type': 'application/json' });
-				res.write(JSON.stringify(data));
-				res.end();
-			});
-		} catch (error) {
-			console.log("- error :>> ", error);
-			res.writeHead(400, { "Content-Type": "text/html" });
-			res.end(`<h1>${res.statusCode} - ${error}</h1>`);
+app.use(express.json());
+// app.use(requestLogger);
+
+const unknownEndpoint = (request, response) => {
+	response.status(404).send({ error: 'unknown endpoint' });
+};
+
+app.get('/', (request, response) => {
+	response.send('<h1>Nothing to display for this endpoint</h1>');
+});
+
+app.get('/api/info', (request, response) => {
+	response.setHeader('date', new Date());
+	response.send(`<p>The phonebook has ${persons.length} people.<br>${response.getHeaders().date}</p>`);
+});
+
+app.get('/api/persons', (request, response) => {
+	response.json(persons);
+});
+
+app.post('/api/persons', (request, response) => {
+	const newPerson = {
+		id: Math.round(Math.random() * 10000000),
+		name: "John Doe",
+		number: Math.round(Math.random() * 10000000000000).toString()
+	};
+	var done = finalhandler(request, response);
+	var logger = morgan(':method :url :status :res[content-length] - :response-time ms :personAdded');
+	morgan.token('personAdded', function getPersonAdded(request) {
+		const personAdded = persons.find(person => person.name === newPerson.name);
+		const { id, ...personDetails } = personAdded; // get person without  id prop for person details
+		return JSON.stringify(personDetails);
+	});
+
+	logger(request, response, function (err) { // morgan
+
+
+		if (!newPerson.name) {
+			response.status(400).send({ error: "no name given" });
 			return;
 		}
-	}
 
-	async function getInfo() {
-		try {
-			const response = await fetch('http://localhost:3002/api/info', {
-				method: "GET",
-				headers: { "Content-Type": "application/json" }
-			});
-			if (response.status !== 200) {
-				throw new Error();
-			}
-			const data = await response.json();
-			const dateRequested = response.headers.get('date');
-			res.writeHead(200, { 'Content-Type': 'text/html' });
-			res.write(`<p>Phonebook has info for ${data} people.<br><br>${dateRequested}</p>`);
-			res.end();
-		} catch (error) { pathNotFound(); }
-	}
+		if (!newPerson.number) {
+			response.status(400).send({ error: "no number given" });
+			return;
+		}
 
-	async function pathNotFound() {
-		res.writeHead(404, { 'Content-Type': 'text/html' });
-		res.end(`<h1>404 -Not Found</h1>`);
-		return;
+		if (persons.find(entry => entry.name === newPerson.name)) {
+			response.status(400).send({ error: `name must be unique` });
+			return;
+		}
+
+		persons = persons.concat(newPerson);
+		response.json(newPerson);
+	});
+});
+
+app.get('/api/persons/:id', (request, response) => {
+	const id = Number(request.params.id);
+	const person = persons.find(person => person.id === id);
+	if (person) {
+		response.json(person);
+	} else {
+		response.status(404).end();
 	}
 });
 
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`);
+app.delete('/api/persons/:id', (request, response) => {
+	const id = Number(request.params.id);
+	persons = persons.filter(person => person.id !== id);
+
+	response.status(204).end();
 });
+
+app.use(unknownEndpoint);
+
+app.listen(3001);
+
+// const PORT = process.env.PORT || 3001;
+// app.listen(PORT, () => {
+// 	console.log(`Server running on port ${PORT}`);
+// });
