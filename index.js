a968aed04999f941fe52b7f30eef15c95a95115f
Coderperson101 <richardtbohnen@yahoo.com>
2024-06-15 14:54:42 +0200

    DLed Fullstack course notes

--- index.js
+++ index.js
@@ -1,109 +1,105 @@
-// --------------------------------------------------
-// #region Imports
-
-require('dotenv').config()
 const express = require('express')
 const app = express()
-var morgan = require('morgan')
-var finalhandler = require('finalhandler')
-const cors = require('cors')
-const mongoose = require('mongoose')
-const Person = require('./models/persons.js')
-const common = require('common.min.js')
+require('dotenv').config()
 
-// #endregion Imports
-// --------------------------------------------------
-
-
-
-// --------------------------------------------------
-// #region Error Handling
-
-const errorHandler = (error, request, response, next) => {
-	console.error(error.message)
-	if (error.name === 'CastError') {
-		return response.status(400).send({ error: 'malformatted id' })
-	}
-	next(error)
-}
-
-const unknownEndpoint = (request, response) => {
-	response.status(404).send({ error: 'unknown endpoint' })
-}
-
-// #endregion Error Handling
-// --------------------------------------------------
-
-
-
-// --------------------------------------------------
-// #region App Assigning
+const Note = require('./models/note')
 
 app.use(express.static('dist'))
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+const cors = require('cors')
+
 app.use(cors())
 app.use(express.json())
+app.use(requestLogger)
 
-// #endregion App Assigning
-// --------------------------------------------------
-
-
-
-// --------------------------------------------------
-// #region Fetches
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
 app.get('/', (request, response) => {
-	response.send('<h1>Nothing to display for this endpoint</h1>')
+  response.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/persons', (request, response) => {
-	Person.find({}).then(persons => {
-		response.json(persons)
-	})
+app.get('/api/notes', (request, response) => {
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
-app.get('/api/persons/:id', (request, response, next) => {
-	Person.findById(request.params.id)
-		.then(person => {
-			if (person) {
-				response.json(person)
-			} else {
-				response.status(404).end()
-			}
-		})
-		.catch(error => next(error))
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
+  }
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  })
+
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
-app.delete('/api/persons/:id', (request, response, next) => {
-	Person.findByIdAndDelete(request.params.id)
-		.then(result => {
-			response.status(204).end()
-		})
-		.catch(error => next(error))
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
-// #endregion Fetches
-// --------------------------------------------------
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
 
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
 
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
 
-// --------------------------------------------------
-// #region APP Assign Errors
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
 
 app.use(unknownEndpoint)
 app.use(errorHandler)
 
-// #endregion Assign Errors
-// --------------------------------------------------
-
-
-
-// --------------------------------------------------
-// #region Port Listeners
-
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
-
-// #endregion Port Listeners
-// --------------------------------------------------
